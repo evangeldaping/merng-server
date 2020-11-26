be67624835f13a256d3d28df0d99120a1e62410c
evangel <evangeldaping@gmail.com>
2020-11-26 01:07:07 -0800

    modified files

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,26 @@
 const { model, Schema } = require('mongoose');
 
-const poastSchema = new Schema({
-    body: String,
-    username: String,
-    createdAt: String,
-    comments: [
-        {
-            body: String,
-            username: String,
-            createdAt: String
-        }
-    ],
-    likes: [
-        {
-            username: String,
-            createdAt: String
-        }
-    ],
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+const postSchema = new Schema({
+  body: String,
+  username: String,
+  createdAt: String,
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String
     }
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String
+    }
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
 });
 
-module.exports = model('Post', poastSchema);
+module.exports = model('Post', postSchema);
